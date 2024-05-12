7812efac5872f0373e95a44f9004677bb58f8977
alfeuduran <alfeuduran@gmail.com>
2024-05-12 07:34:37 -0300

    test pytest

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,12 @@
+import pytest
 from bundlesDevOpsDemo.main import get_taxis, get_spark
 
+@pytest.fixture
+def setup_taxis():
+    spark_session = get_spark()
+    taxis = get_taxis(spark_session)
+    return taxis
 
-def test_main():
-    taxis = get_taxis(get_spark())
-    assert taxis.count() > 5
+def test_taxi_count(setup_taxis):
+    # setup_taxis is the taxis DataFrame returned by the fixture
+    assert setup_taxis.count() > 5
